e25ee358cf6ed7cd0eeca4bd2dfea8e925776f68
Riley Durbin <radurbin@crimson.ua.edu>
2024-07-23 13:25:59 +0100

    + Display total usage time

--- DeviceActivity-main/MonitorReport/TotalActivityReport.swift
+++ DeviceActivity-main/MonitorReport/TotalActivityReport.swift
@@ -10,18 +10,13 @@ import DeviceActivity
 import SwiftUI
 
 extension DeviceActivityReport.Context {
-    // If your app initializes a DeviceActivityReport with this context, then the system will use
-    // your extension's corresponding DeviceActivityReportScene to render the contents of the
-    // report.
     static let totalActivity = Self("Atividades")
 }
 
 struct TotalActivityReport: DeviceActivityReportScene {
     
-    // Define which context your scene will represent.
     let context: DeviceActivityReport.Context = .totalActivity
     
-    // Define the custom configuration and the resulting view for this report.
     let content: (DeviceActivity) -> TotalActivityView
     
     func makeConfiguration(representing data: DeviceActivityResults<DeviceActivityData>) async -> DeviceActivity {
@@ -47,5 +42,4 @@ struct TotalActivityReport: DeviceActivityReportScene {
         
         return DeviceActivity(duration: totalActivityDuration, apps: list)
     }
-    
 }
--- DeviceActivity-main/MonitorReport/Views/ActivitiesView.swift
+++ DeviceActivity-main/MonitorReport/Views/ActivitiesView.swift
@@ -15,8 +15,8 @@ struct ActivitiesView: View {
         VStack {
             Spacer(minLength: 50)
             
-            // Display total usage time
-            Text("Usage time: \(formatTotalUsageTime(activities.apps))")
+            // Display total usage time from DeviceActivity
+            Text("Usage time: \(formatTotalUsageTime(activities.duration))")
                 .bold()
                 .font(.title3)
             
@@ -26,12 +26,12 @@ struct ActivitiesView: View {
         }
     }
     
-    // Function to calculate and format total usage time
-    private func formatTotalUsageTime(_ apps: [AppReport]) -> String {
-        let totalSeconds = apps.reduce(0) { $0 + $1.duration }
-        let hours = Int(totalSeconds) / 3600
-        let minutes = (Int(totalSeconds) % 3600) / 60
-        let seconds = Int(totalSeconds) % 60
+    // Function to format total usage time
+    private func formatTotalUsageTime(_ duration: TimeInterval) -> String {
+        let totalSeconds = Int(duration)
+        let hours = totalSeconds / 3600
+        let minutes = (totalSeconds % 3600) / 60
+        let seconds = totalSeconds % 60
         
         return String(format: "%dh %02dm %02ds", hours, minutes, seconds)
     }
@@ -39,6 +39,6 @@ struct ActivitiesView: View {
 
 struct ActivitiesView_Previews: PreviewProvider {
     static var previews: some View {
-        ActivitiesView(activities: DeviceActivity(duration: .zero, apps: [AppReport(id: "1", name: "Twitter", duration: .zero)]))
+        ActivitiesView(activities: DeviceActivity(duration: 3422, apps: [AppReport(id: "1", name: "Twitter", duration: 600)]))
     }
 }
