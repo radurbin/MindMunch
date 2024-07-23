4079e4662924a45cc89eeeaf46cefc96c04cc292
Riley Durbin <radurbin@crimson.ua.edu>
2024-07-23 13:24:48 +0100

    + Sum up total usage time

--- DeviceActivity-main/MonitorReport/Views/ActivitiesView.swift
+++ DeviceActivity-main/MonitorReport/Views/ActivitiesView.swift
@@ -9,23 +9,32 @@
 import SwiftUI
 
 struct ActivitiesView: View {
-    
     var activities: DeviceActivity
     
     var body: some View {
-        
         VStack {
             Spacer(minLength: 50)
-            Text("Usage time: ")
+            
+            // Display total usage time
+            Text("Usage time: \(formatTotalUsageTime(activities.apps))")
                 .bold()
                 .font(.title3)
+            
             List(activities.apps) { app in
                 ListItem(app: app)
             }
         }
-        
     }
     
+    // Function to calculate and format total usage time
+    private func formatTotalUsageTime(_ apps: [AppReport]) -> String {
+        let totalSeconds = apps.reduce(0) { $0 + $1.duration }
+        let hours = Int(totalSeconds) / 3600
+        let minutes = (Int(totalSeconds) % 3600) / 60
+        let seconds = Int(totalSeconds) % 60
+        
+        return String(format: "%dh %02dm %02ds", hours, minutes, seconds)
+    }
 }
 
 struct ActivitiesView_Previews: PreviewProvider {
